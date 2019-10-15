de4bfda4d665cf2af5c7c0a232a913eef619ead8
Patrick Labbett <patrick@labbett.net>
2019-10-15 03:18:06 -0400

    fixed typo, small reformat

--- source/_layouts/footer.blade.php
+++ source/_layouts/footer.blade.php
@@ -5,10 +5,10 @@
         <span class="text-gray-600 opacity-75">All rights reserved &copy; 2019</span>
     </p>
     <p class="text-xs font-sans w-100 text-gray-700 mt-10">
-        Bult using <a class="underline hover:text-gray-600" href="https://jigsaw.tighten.co">Jigsaw</a>,
+        Built using <a class="underline hover:text-gray-600" href="https://jigsaw.tighten.co">Jigsaw</a>,
         <a class="underline hover:text-gray-600" href="https://tailwindcss.com">TailwindCSS</a>, 
-        <a class="underline hover:text-gray-600" href="https://fontawesome.com">Font Awesome</a>,
-        <a class="underline hover:text-gray-600" href="https://undraw.co">unDraw</a>, 
-        and hosted by <a class="underline hover:text-gray-600" href="https://www.netlify.com">Netlify</a>.
+        <a class="underline hover:text-gray-600" href="https://fontawesome.com">Font Awesome</a>, and 
+        <a class="underline hover:text-gray-600" href="https://undraw.co">unDraw</a>.
+        Hosted by <a class="underline hover:text-gray-600" href="https://www.netlify.com">Netlify</a>.
     </p>
 </div>
