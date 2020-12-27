b56c0547f0a649c30fd51accb99bb4b116fe9917
Patrick Labbett <patrick@labbett.net>
2020-12-26 20:43:58 -0500

    Updated not found page

--- source/404.html.blade.php
+++ source/404.html.blade.php
@@ -3,20 +3,14 @@
 @section('title', 'Not Found')
 
 @section('body')
-    <div class="container px-4 mx-auto max-w-lg text-center pt-12">
-        <a href="/" class="text-indigo-300 hover:text-indigo-500">
-            <h1 class="text-white text-5xl uppercase text-center font-sans">
-            404 <span class="text-gray-300">Not Found</span>
-        </h1>
-        </a>
-    </div>
 
-    <div class="container px-4 max-w-sm mx-auto pt-12 text-center">
-        <div class="w-100 my-24">
+    <div class="container max-w-md px-4 mx-auto text-center my-24">
 
-                <img class="w-auto mx-auto mb-4" src="/assets/images/void.svg" title="Go home" alt="Alien abduction">
-            </a>
-        </div>
+            <img class="inline max-w-sm mx-auto mb-4" src="/assets/images/voice.svg" title="Nothing found here..." alt="Void">
+
+            <p class="font-hairline text-2xl font-sans">
+                <a href="/" class="text-gray-300 hover:text-white align-middle">Try this instead?</a>
+            </p>
     </div>
 
 @endsection
